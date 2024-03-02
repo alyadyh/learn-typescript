c1dfffc5d0f2e30d6e1838517bd3a7ccd99d276d
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-02 10:01:50 +0700

    arrays & objects

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,54 @@
 "use strict";
-let myName = 'Alya';
-let meaningOfLife;
-let isLoading;
-let album;
-myName = 'Dhiya';
-meaningOfLife = 23;
-isLoading = true;
-album = 1234;
-const sum = (a, b) => {
-    return a + b;
+let guitars = ['Strat', 'Les Paul', 5150];
+guitars[0] = 2003;
+let stringArr = ['one', 'hey', 'Alya'];
+stringArr.push('halo');
+let mixedData = ['EVH', 1984, true];
+mixedData.push(false);
+mixedData = guitars;
+// cannot in reverse
+// guitars = mixedData -> cuz guitars only accept datatype string and number
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+// Tuple
+let myTuple = ['Alya', 21, true];
+let mixed = ['Hey', 1, false];
+mixed = myTuple;
+// myTuple = mixed -> can't be run cuz myTuple is more strict
+// Object
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: 'Alya',
+    prop2: true,
 };
-let postId;
-let isActive;
-let re = /\w+/g;
+exampleObj.prop1 = 'Dhiya';
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+let jp = {
+    name: 'Jimmy',
+    active: true,
+    albums: ['I', 'II', 'IV']
+};
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}`;
+};
+console.log(greetGuitarist(jp));
+// Enums
+// "Unlike most TypeScript features, enums are not a type-level
+// addition to JavaScript but something added to the language and runtime"
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,75 @@
-let myName: string = 'Alya'
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let guitars = ['Strat', 'Les Paul', 5150]
+guitars[0] = 2003
 
-myName = 'Dhiya'
-meaningOfLife = 23
-isLoading = true
-album = 1234
+let stringArr = ['one', 'hey', 'Alya']
+stringArr.push('halo')
 
-const sum = (a: number, b: string) => {
-    return a + b
+let mixedData = ['EVH', 1984, true]
+mixedData.push(false)
+
+mixedData = guitars
+
+// cannot in reverse
+// guitars = mixedData -> cuz guitars only accept datatype string and number
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple
+let myTuple: [string, number, boolean] = ['Alya', 21, true]
+let mixed = ['Hey', 1, false]
+
+mixed = myTuple
+
+// myTuple = mixed -> can't be run cuz myTuple is more strict
+
+// Object
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+    prop1: 'Alya',
+    prop2: true,
 }
 
-let postId: string | number
-let isActive: number | boolean
-let re: RegExp = /\w+/g
+exampleObj.prop1 = 'Dhiya'
+
+type Guitarist = {
+    name: string,
+    active?: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+    name: 'Jimmy',
+    active: true,
+    albums: ['I', 'II', 'IV']
+}
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    return `Hello ${guitarist.name}`
+}
+
+console.log(greetGuitarist(jp))
+
+// Enums
+// "Unlike most TypeScript features, enums are not a type-level
+// addition to JavaScript but something added to the language and runtime"
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
