2ea64534118421a25d91aaa66fa338d0c58827c0
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-02 15:42:51 +0700

    assertions

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,1 @@
 "use strict";
-// Literal types
-let myName;
-let userName;
-userName = 'Amy';
-// Functions
-const sum = (a, b) => {
-    return a + b;
-};
-const logMsg = (message) => {
-    console.log(message);
-};
-logMsg('Hello');
-logMsg(sum(2, 3));
--- src/main.ts
+++ src/main.ts
@@ -1,90 +1,33 @@
-// Type Aliases
-type stringOrNumber = string | number
+type One = string
+type Two = string | number
+type Three = 'hello'
 
-type stringOrNumberArray = (string | number)[]
+// convert to more or less specific
+let a: One = 'hello'
+let b = a as Two //less specific
+let c = a as Three //more specific
 
-type Guitarist = {
-    name?: string,
-    active: boolean,
-    albums: stringOrNumberArray
+let d = <One>'world'
+let e = <string | number>'world'
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
+    if (c === 'add') return a + b
+    return '' + a + b
 }
 
-type UserId = stringOrNumber
+let myVal: string = addOrConcat(2, 2, 'concat') as string
 
-// Literal types
-let myName: 'Alya'
-let userName: 'Alya' | 'Dave' | 'Amy'
-userName = 'Amy'
+// Be careful cuz TS sees no problem - but a string is returned
+let nextVal: number = addOrConcat(2, 2, 'concat') as number
 
-// Functions
-const sum = (a: number, b: number): number => {
-    return a + b
-}
+// 10 as string
+(10 as unknown) as string
 
-const logMsg = (message: any): void => {
-    console.log(message)
-}
+// The DOM
+const img = document.querySelector('img')!
+const myImg = document.getElementById('#img') as HTMLImageElement
+const nextImg = <HTMLImageElement>document.getElementById('#img')
 
-logMsg('Hello')
-logMsg(sum(2,3))
+img.src
+myImg.src
 
-let substract = function (c: number, d: number): number {
-    return c - d
-}
-
-type mathFunction = (a: number, b: number) => number
-// interface mathFunction {
-//     (a: number, b: number) : number
-// }
-
-let multiply: mathFunction = function(c, d) {
-    return c * d
-}
-
-// optional parameters
-const addAll = (a: number, b: number, c?: number): 
-number => {
-    if (typeof c !== 'undefined') {
-        return a + b + c
-    }
-    return a + b
-}
-
-// default parameters value
-const sumAll = (a: number, b: number, c: number = 2): 
-number => {
-    return a + b
-}
-
-// Rest Parameters
-const total = (a: number, ...nums: number[]): number => {
-    return a + nums.reduce((prev, curr) => prev + curr)
-}
-
-logMsg(total(10, 2, 3))
-
-const createError = (errMsg: string): never => {
-    throw new Error(errMsg)
-}
-
-const infinite = () => {
-    let i: number = 1
-    while (true) {
-        i++
-        if (i > 100) break
-    }
-}
-
-// custom type guard
-const isNumber = (value: any): boolean => {
-    return typeof value === 'number'
-    ? true : false
-}
-
-// use of the never type
-const numberOrString = (value: number | string):
-string => {
-    if (typeof value === 'string') return 'string'
-    if (typeof value === 'number') return 'number'
-    return createError(`This should never happen!`)
-}
